6aee5181616c46a782ef6153a04f0a9354fb7644
WassCodeur <wachioubouraima56@gmail.com>
2023-05-26 01:07:22 +0000

    export from an api to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("USAGE : pyton3 1-export_to_CSV.py userId\n or")
+        print("./1-export_to_CSV.py userId")
+    else:
+        userId = argv[1]
+        filename = "{}.csv".format(userId)
+        urlTodos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+        urlUsers = "https://jsonplaceholder.typicode.com/users/{}"
+        resUser = requests.get(urlUsers.format(userId))
+        user = resUser.json()
+        usrname = user['username']
+        resTodos = requests.get(urlTodos.format(userId))
+        todos = resTodos.json()
+        for td in todos:
+            cplt = td['completed']
+            title = td['title']
+            line = '"{}","{}","{}","{}"\n'.format(userId, usrname, cplt, title)
+            with open(filename, 'a') as f:
+                f.write(line)
