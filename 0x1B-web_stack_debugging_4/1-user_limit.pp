275f5f54fc50e3f9f5b2fc2492f05b0546b55998
WassCodeur <wachioubouraima56@gmail.com>
2023-06-16 02:59:43 +0000

    advanced task

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# increase user limit
+
+exec{'harrd-limit':
+	command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits/conf',
+	path    => '/usr/bin/env'
+}
+ exec{'soft-limit':
+           command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits/conf',
+           path    => '/usr/bin/env'
+   }
