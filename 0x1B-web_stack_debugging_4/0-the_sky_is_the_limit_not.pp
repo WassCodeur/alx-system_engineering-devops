cc54ff9b114ff8681d3810b252db8d175bf90a9a
WassCodeur <wachioubouraima56@gmail.com>
2023-06-16 03:08:50 +0000

    update task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Sky is the limit, let's bring that limit higher
 exec{'IncreaseLimit':
-	command => 'sed -i s/15/3000/ /etc/default/nginx',
+	command => 'sed -i "s/15/3000/" /etc/default/nginx',
 	path => '/usr/bin/env'
 }
 exec{'restartNginx':
