e555e81dd1b20cc8182068e2ce471f84702b0d32
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-06-23 07:50:18 +0000

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,6 @@
 # increase user limit
 
-exec{'harrd-limit':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+exec{'hard-limit':
+  command => 'sed -i "s/4/20000/; s/5/20000" /etc/security/limits.conf',
   path    => '/usr/bin/env'
 }
-exec{'soft-limit':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits/conf',
-  path    =>'/usr/bin/env'
-  }
